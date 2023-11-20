cfaed5c87597083a8d4395dd8c80db3a2c6d8e9b
Marc Ridler <44672709+marcridler@users.noreply.github.com>
2023-11-20 17:13:28 +0100

    fixes.

--- pyfme/io/xlsx.py
+++ pyfme/io/xlsx.py
@@ -1,6 +1,7 @@
 """The simulation module"""
 import pandas as pd
 from ..dataset.dataset_model import DatasetModel
+from typing import Dict
 
 
 class Xlsx:
@@ -20,8 +21,9 @@ class Xlsx:
         """
         self._filename = filename
         self._datamodel = datamodel
+        self._data = {}
 
-    def read(self, strict=False) -> dict(str, pd.DataFrame):
+    def read(self, strict=False) -> Dict[str, pd.DataFrame]:
         """read the xlsx file
 
         Parameters
@@ -32,19 +34,24 @@ class Xlsx:
         -------
         dictionary of panda dataframes
         """
-        if self._datamodel == None:
-            data = pd.read_excel(io=self._filename, sheet_name=None)
+        if self._datamodel is None:
+            self._data = pd.read_excel(io=self._filename, sheet_name=None)
         else:
-            data = {}
+            self._data = {}
             for table_name in self._datamodel.table_names:
                 table = self._datamodel.get_table(table_name)
                 if not table:
                     raise ValueError(
                         f"Error. Could not find table {table_name} in excel file."
                     )
-                data[table_name] = pd.read_excel(
+                self._data[table_name] = pd.read_excel(
                     io=self._filename,
                     parse_dates=table.date_columns,
                     dtype=table.non_date_fields,
                     sheet_name=table_name,
                 )
+        return self._data
+
+    @property
+    def dataset(self) -> Dict[str, pd.DataFrame]:
+        return self._data
--- tests/test_xlsx.py
+++ tests/test_xlsx.py
@@ -1,7 +1,32 @@
+import json
+import os
+import pytest
 from pyfme import Xlsx
+from pyfme import DatasetModel
 
 
-def test_fake_xlsx():
-    xlsx = Xlsx(name="Test")
-    numbers = xlsx.read(1)
-    assert numbers is not None
+@pytest.fixture
+def nitrate_schema():
+    f = os.path.join(os.getcwd(), "tests/data/nitrate_schema.json")
+    j = json.load(open(f))
+    dm = DatasetModel()
+    dm._read_schema(j)
+
+    return dm
+
+
+@pytest.fixture
+def filename_xlsx():
+    return os.path.join(os.getcwd(), "tests/data/fake_italy_reporting_tiny.xlsx")
+
+
+def test_read_xlsx_without_schema(filename_xlsx):
+    xlsx = Xlsx(filename=filename_xlsx)
+    data = xlsx.read()
+    assert xlsx is not None
+
+
+def test_read_xlsx_with_schema(nitrate_schema, filename_xlsx):
+    xlsx = Xlsx(filename=filename_xlsx, datamodel=nitrate_schema)
+    data = xlsx.read()
+    assert xlsx is not None
